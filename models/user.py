e75ca30eb751651b0d4af1731f8616313b66ed04
michaeljumah <michaeljumah8@gmail.com>
2023-09-20 13:14:31 -0700

    removed the base imported from base_model.py....will add after implimenting it

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+
+from models.base_model import BaseModel
 from sqlalchemy import column, string
 import models
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """ User Representation """
 
     __tablename__ = 'users'
