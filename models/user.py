f24aed2e3359ce89943d73ed9cda214d79e3e009
KennedyRyan-code <kenryan49@gmail.com>
2023-09-20 15:56:05 +0300

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import column, string
-import hashlib
 import models
 
 
 class User(BaseModel, Base):
     """ User Representation """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
         __tablename__ = 'users'
         email = Column(String(128),
                        nullable=False)
-        password = Column('password',
-                           String(128),
+        password = Column(String(128),
                            nullable=False)
         first_name = Column(String(128),
                             nullable=True)
         last_name = Column(String(128),
                            nullable=True)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        """initialize user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password value"""
-        self._password = pwd
