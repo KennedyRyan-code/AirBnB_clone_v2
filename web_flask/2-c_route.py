6f9bc31677901280f9c25a5731f61caacd686555
KennedyRyan-code <kenryan49@gmail.com>
2023-10-22 15:53:51 +0300

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace("_", " ")
+    return (f"C {escape(text)}")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
