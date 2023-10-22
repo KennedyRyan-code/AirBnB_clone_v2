2e69f72efd0b989d7715226f327db1ff2ea7e635
KennedyRyan-code <kenryan49@gmail.com>
2023-10-22 16:04:09 +0300

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,7 +3,7 @@
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     text = text.replace("_", " ")
-    return f"C {escape(text)}"
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
